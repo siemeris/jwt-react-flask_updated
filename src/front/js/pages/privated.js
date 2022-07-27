d1cd3e5409306d95c5ce521c4dbcea1846c38d7f
siemeris <siemeris@gmail.com>
2022-07-27 14:29:12 +0000

    falta revisar privated.js

--- src/front/js/pages/privated.js
+++ src/front/js/pages/privated.js
@@ -11,6 +11,7 @@ export const Privated = () => {
    useEffect(() => {
     actions.private();
   }, []); 
+
         return (
             <>
                 {store.token != null && store.auth == true? 
@@ -24,50 +25,3 @@ export const Privated = () => {
         )
 
 };
-//&& store.auth == true
-
-// asumiendo que "/protected" es un endpoint privado
-// const getMyTasks = await (username, password) => {
-//     // retrieve token form localStorage
-//     const token = localStorage.getItem('token');
-
-//     const resp = await fetch(`https://your_api.com/protected`, {
-//        method: 'GET',
-//        headers: { 
-//          "Content-Type": "application/json",
-//          'Authorization': 'Bearer '+token // ⬅⬅⬅ authorization token
-//        } 
-//     })
-//     if(!resp.ok) throw Error("There was a problem in the login request")
-
-//     else if(resp.status === 403){
-//         throw Error("Missing or invalid token");
-//     }
-//     else{
-//         throw Error('Uknon error');
-//     }
-
-//     const data = await resp.json();
-//     console.log("This is the data you requested", data);
-//     return data
-
-// }
-
-
-
-// export const Privated = () => {
-//     const {store, actions} = useContext(Context)
-//     const token = localStorage.getItem('token');
-//         return (
-//             <>
-//                 {store.token != null && store.auth == true?
-//                     <div className="text-center"> 
-//                         <h1>Página personal que ha realizado la autentificacion jwt_require</h1>
-//                         {/* <Link className="btn btn-primary" to="/privates">Home</Link> */}
-//                     </div>
-//                 :<Navigate to="/"/> 
-//                 }
-//             </>
-//         )
-
-// };
